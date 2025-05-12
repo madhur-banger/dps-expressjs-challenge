d370895f35b46648c054ad34ce38ea6073cc9dec
madhur-banger <madhur.cloudevops@gmail.com>
2025-05-12 21:11:10 +0530

    Updated Dist Folder

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 export function errorHandler(
 	err: Error,
 	_req: Request,
 	res: Response,
-	// _next: NextFunction,
+	_next: NextFunction, // eslint-disable-line @typescript-eslint/no-unused-vars
 ) {
 	console.error(err.stack);
 	res.status(500).json({ message: err.message });
